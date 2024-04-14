bf9cabb7a08fa16280708af31d38d27ad5f618f2
Konstanty Cyran <96084033+krixlion@users.noreply.github.com>
2024-04-14 17:19:24 +0200

    fix: remove double import of the same module

--- pkg/grpc/deserialize/key.go
+++ pkg/grpc/deserialize/key.go
@@ -10,7 +10,6 @@ import (
 	"strconv"
 
 	ecPb "github.com/krixlion/dev_forum-auth/pkg/grpc/v1/ec"
-	rsaPb "github.com/krixlion/dev_forum-auth/pkg/grpc/v1/rsa"
 	rsapb "github.com/krixlion/dev_forum-auth/pkg/grpc/v1/rsa"
 	"google.golang.org/protobuf/proto"
 )
@@ -23,7 +22,7 @@ var (
 // Key detects a gRPC format of key and deserializes it using the corresponding function.
 func Key(input proto.Message) (interface{}, error) {
 	switch msg := input.(type) {
-	case *rsaPb.RSA:
+	case *rsapb.RSA:
 		return RSA(msg)
 	case *ecPb.EC:
 		return ECDSA(msg)
