827e9520f1a40d934f466db27df7d8a57826a25a
Konstanty Cyran <96084033+krixlion@users.noreply.github.com>
2024-04-14 17:09:09 +0200

    style: merge public key deserialize funcs definitions into one file

--- pkg/grpc/deserialize/key.go
+++ pkg/grpc/deserialize/key.go
@@ -1,10 +1,17 @@
 package deserialize
 
 import (
+	"crypto/ecdsa"
+	"crypto/elliptic"
+	"crypto/rsa"
+	"encoding/base64"
 	"errors"
+	"math/big"
+	"strconv"
 
 	ecPb "github.com/krixlion/dev_forum-auth/pkg/grpc/v1/ec"
 	rsaPb "github.com/krixlion/dev_forum-auth/pkg/grpc/v1/rsa"
+	rsapb "github.com/krixlion/dev_forum-auth/pkg/grpc/v1/rsa"
 	"google.golang.org/protobuf/proto"
 )
 
@@ -23,4 +30,82 @@ func Key(input proto.Message) (interface{}, error) {
 	default:
 		return nil, ErrUnknownMessageType
 	}
+}
+
+// Unserializes an RSA message.
+func RSA(input *rsapb.RSA) (*rsa.PublicKey, error) {
+	if input == nil {
+		return nil, ErrKeyNil
+	}
+
+	n, err := base64.RawURLEncoding.DecodeString(input.GetN())
+	if err != nil {
+		return nil, err
+	}
+
+	N := new(big.Int).SetBytes(n)
+
+	// The short path.
+	// The default exponent is usually 65537, so just compare the base64 for [1,0,1] or [0,1,0,1].
+	if input.GetE() == "AQAB" || input.GetE() == "AAEAAQ" {
+		return &rsa.PublicKey{
+			E: 65537,
+			N: N,
+		}, nil
+	}
+
+	eBytes, err := base64.RawURLEncoding.DecodeString(input.GetE())
+	if err != nil {
+		return nil, err
+	}
+
+	E, err := strconv.Atoi(string(eBytes))
+	if err != nil {
+		return nil, err
+	}
+
+	return &rsa.PublicKey{
+		N: N,
+		E: E,
+	}, nil
+}
+
+// Unserializes an ECDSA message.
+func ECDSA(input *ecPb.EC) (*ecdsa.PublicKey, error) {
+	if input == nil {
+		return nil, ErrKeyNil
+	}
+
+	x, err := base64.RawURLEncoding.DecodeString(input.GetX())
+	if err != nil {
+		return nil, err
+	}
+
+	y, err := base64.RawURLEncoding.DecodeString(input.GetY())
+	if err != nil {
+		return nil, err
+	}
+
+	X := new(big.Int).SetBytes(x)
+	Y := new(big.Int).SetBytes(y)
+
+	var crv elliptic.Curve
+	switch input.GetCrv() {
+	case ecPb.ECType_P256:
+		crv = elliptic.P256()
+	case ecPb.ECType_P384:
+		crv = elliptic.P384()
+	case ecPb.ECType_P521:
+		crv = elliptic.P521()
+	case ecPb.ECType_UNDEFINED:
+		return nil, errors.New("failed to parse ECDSA key: curve undefined")
+	default:
+		return nil, errors.New("failed to parse ECDSA key: unexpected curve")
+	}
+
+	return &ecdsa.PublicKey{
+		Curve: crv,
+		X:     X,
+		Y:     Y,
+	}, nil
 }
