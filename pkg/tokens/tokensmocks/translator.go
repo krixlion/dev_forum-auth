bfa08538338b17863967fe22ecad8170c6caa946
Konstanty Cyran <96084033+krixlion@users.noreply.github.com>
2024-07-19 01:20:31 +0200

    feat: add token translator mock

---
+++ pkg/tokens/tokensmocks/translator.go
@@ -0,0 +1,24 @@
+package tokensmocks
+
+import (
+	"github.com/krixlion/dev_forum-auth/pkg/tokens"
+	"github.com/krixlion/dev_forum-auth/pkg/tokens/translator"
+	"github.com/stretchr/testify/mock"
+)
+
+var _ tokens.Translator = (*translator.Translator)(nil)
+
+type TokenTranslator struct {
+	*mock.Mock
+}
+
+func NewTokenTranslator() TokenTranslator {
+	return TokenTranslator{
+		Mock: new(mock.Mock),
+	}
+}
+
+func (m TokenTranslator) TranslateAccessToken(opaqueAccessToken string) (string, error) {
+	args := m.Called(opaqueAccessToken)
+	return args.String(0), args.Error(1)
+}
